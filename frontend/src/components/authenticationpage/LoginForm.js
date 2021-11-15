6ae7a940de619bc2f2349d9553351ff48f960827
Samuelcodingg <samuelroman39@gmail.com>
2021-11-15 17:21:24 -0500

    retoques al form registro, front ok, falta consumir api

--- frontend/src/components/authenticationpage/LoginForm.js
+++ frontend/src/components/authenticationpage/LoginForm.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import logo from '../ui/Logo.png';
 
 
 export const LoginForm = () => {
+
+    const [values, setValues] = useState({
+        correo_empresa: '',
+        password: '',
+        redirectToReferrer: false
+    });
+
+    const { correo_empresa, password, redirectToReferrer } = values;
+
+    const handleChange = name => event => {
+        setValues({ ...values, [name]: event.target.value });
+    };
+
+    const clickSubmit = event => {
+        event.preventDefault();
+        console.log('values', values);
+    };
+
+    const redirectUser = () => {
+        if (redirectToReferrer) {
+            return <Redirect to='/' />
+        }
+    }
+
     return (
         <>
             <img src={logo} alt="logo" className="img-fluid" />
@@ -12,18 +36,35 @@ export const LoginForm = () => {
                 <div className="row">
                     <form className="col-md-6 mx-auto">
                         <div className="form-group mt-5">
-                            <input type="email" className="form-control remove-focus just-bottom-border" placeholder="Email" />
+                            <input 
+                                onChange={handleChange('correo_empresa')}
+                                type="email" 
+                                className="form-control remove-focus just-bottom-border" 
+                                placeholder="Email"
+                                value={correo_empresa}  
+                            />
                         </div>
                         <div className="form-group mt-4">
-                            <input type="password" placeholder="Contraseña" className="form-control remove-focus just-bottom-border" />
+                            <input 
+                                onChange={handleChange('password')}
+                                type="password" 
+                                placeholder="Contraseña" 
+                                className="form-control remove-focus just-bottom-border" 
+                                value={password}
+                            />
                         </div>
                         <div className="form-group mt-4">
-                            <button className="w-100 rounded border-btn shadow bg-oscuro text-white py-2 fw-bold">Iniciar sesión</button>
+                            <button 
+                                className="w-100 rounded border-btn shadow bg-oscuro text-white py-2 fw-bold"
+                                onClick={clickSubmit}
+                            >
+                                Iniciar sesión
+                            </button>
                         </div>
                         <div className="d-flex flex-column my-4">
                             <div className="d-flex justify-content-between ">
                                 <Link
-                                    to="/register"
+                                    to="/authentication/register"
                                     className="text-secondary"
                                 >
                                     ¿Aún no estás registrado?
@@ -37,7 +78,7 @@ export const LoginForm = () => {
                             </div>
                             <div className="mt-3">
                                 <Link
-                                    to="/"
+                                    to="/authentication/register"
                                     className="text-secondary"
                                 >
                                     ¿Eres una nueva empresa?
--- frontend/src/components/homepage/HomePage.js
+++ frontend/src/components/homepage/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const HomePage = () => {
     return (
         <div>
-            <h1>HomePage</h1>
+            <h1>Autenticado !</h1>
         </div>
     )
 }
