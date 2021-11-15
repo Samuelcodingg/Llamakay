91c3c83175eb134440516492a264ad7e414704fa
Samuelcodingg <samuelroman39@gmail.com>
2021-11-14 23:41:26 -0500

    navbar iniciado y setup de ruteo en frontend

---
+++ frontend/src/routers/AppRouter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+} from 'react-router-dom';
+import { AuthenticationPage } from '../components/authenticationpage/AuthenticationPage';
+import { NavbarComponent } from '../components/ui/NavbarComponent';
+
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <NavbarComponent />
+
+            <div className="container mt-5">
+                <Routes>
+                    <Route path="/login" element={<AuthenticationPage />} />
+                </Routes>
+
+            </div>
+        </Router>
+    )   
+}
