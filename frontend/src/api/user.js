5b517b1e60307c2dd0873099a9de9c0f3394341f
Samuelcodingg <samuelroman39@gmail.com>
2021-11-17 15:57:11 -0500

    se agregó integración en perfil, petición ok al back

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -1,1 +1,20 @@
-// crear funciones para trabajar la data del usuario
+import { API } from '../config';
+
+export const getUser = async (user) => {
+
+    let usuario = '';
+    let id = '';
+
+    if(user.empresa) {
+        usuario = 'empresas';
+        id = user.empresa._id;
+    }
+    else if(user.alumno) {
+        usuario = 'alumnos';
+        id = user.alumno._id;
+    }
+
+    const response = await fetch(`${API}/auth/${usuario}/${id}`);
+    const data = await response.json();
+    return data;
+}
--- frontend/src/components/perfilpage/PerfilPage.js
+++ frontend/src/components/perfilpage/PerfilPage.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Redirect } from 'react-router';
 import { isAuthenticated } from '../../api/auth';
 import { NavbarComponent } from '../ui/NavbarComponent';
 import { Footer } from '../ui/Footer';
+import { getUser } from '../../api/user';
 
 export const PerfilPage = () => {
 
-    const { token } = isAuthenticated();
+    const { token, alumno, empresa } = isAuthenticated();
 
     const redirectUser = () => {
         if (!token) {
             return <Redirect to="/login" />
         }
     }
+
+    const loadDataUser = () => {
+        getUser({alumno, empresa }).then(data => {
+            console.log(data);
+        })
+    }
+
+    useEffect(() => {
+        loadDataUser();
+    }, []);
 
     return (
      
