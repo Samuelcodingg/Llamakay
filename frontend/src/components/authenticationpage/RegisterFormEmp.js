e75cdb00a7072b5d90e8a047ea241049ff6f939c
Manuel Vallejos <manuel.vallejos@unmsm.edu.pe>
2021-11-20 14:06:43 -0500

    Iniciando Registro de empresa

---
+++ frontend/src/components/authenticationpage/RegisterFormEmp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavbarComponent } from '../ui/NavbarComponent';
+
+export const RegisterFormEmp = () => {
+    return (
+        <div>
+            <NavbarComponent/>
+        </div>
+    )
+}
